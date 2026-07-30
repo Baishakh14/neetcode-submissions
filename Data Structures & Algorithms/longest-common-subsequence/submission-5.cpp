705df1074be7ead082543321ea9184fb2d83d117
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-07-31 02:33:02 +0600

    Add: longest-common-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-5.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+int dp[1005][1005];
+    int longestCommonSubsequence(string a, string b) {
+    a = '1' + a;
+    b = '1' + b;
+    int n = a.size();
+    int m = b.size();
+    memset(dp,0,sizeof(dp));
+    for(int i = 1;i<n;i++)
+    {
+        for(int j = 1;j<m;j++)
+        {
+            if(a[i] == b[j])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                int one = dp[i-1][j];
+                int two = dp[i][j-1];
+                //int three = dp[i-1][j-1];
+                dp[i][j] = max({one,two});
+            }
+        }
+    }
+    return dp[n-1][m-1];
+    }
+};
