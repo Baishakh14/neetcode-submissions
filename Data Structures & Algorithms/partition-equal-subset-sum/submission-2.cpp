2c95142040a719c0db0c625ee12a027d5575b348
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-07-30 16:37:17 +0600

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+int dp[100005];
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+    memset(dp,0,sizeof(dp));
+    int sum = accumulate(nums.begin(),nums.end(),0);
+    if(sum & 1) return false;
+    int mid = sum / 2;
+    dp[0] = 1;
+    for(auto it : nums)
+    {
+        for(int i = sum;i>=it;i--)
+        {
+            if(dp[i - it]) dp[i] = 1;
+        }
+    }
+    return dp[mid];
+    }
+};
