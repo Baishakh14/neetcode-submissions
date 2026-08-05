7a28308c86fa911d1582ea3506ce82ffb4791ec0
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-08-05 14:45:59 +0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& st) {
+        vector<string>now;
+        for(auto it : st)
+        {
+            string hi = it;
+            sort(hi.begin(),hi.end());
+            now.push_back(hi);
+        }
+        map<string,vector<string>>fre;
+        for(int i = 0;i<st.size();i++)
+        {
+            string first = st[i];
+            string second = now[i];
+            fre[second].push_back(first);
+        }
+        vector<vector<string>>ans;
+        for(auto it : fre)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
