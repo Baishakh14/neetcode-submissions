3688990d7b91127a71fa0171e1a84acdf6efa9c9
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-08-19 01:22:57 +0600

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TimeMap {
+public:
+map<string,set<pair<int,string>>>fre;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+      fre[key].insert({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+      if(!fre.count(key)) return "";
+      else 
+      {
+        auto &bai = fre[key];
+        auto it = bai.upper_bound({timestamp,"zzz"});
+        if(it == bai.begin()) return "";
+        else 
+        {
+        it--;
+        return it->second;
+        }
+      }
+    }
+};
