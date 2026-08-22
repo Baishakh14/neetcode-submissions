09a4588e297e22c2888b990e5a7ebb96e41ac14d
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-08-22 22:55:50 +0600

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+static const int mx = 10;
+int n;
+bool vis[mx];
+vector<vector<int>>ans;
+vector<int>now;
+void find(vector<int>&nums)
+{
+  if(now.size() == n)
+  {
+    ans.push_back(now);
+    return;
+  }
+  for(int i = 0;i<n;i++)
+  {
+    if(!vis[i])
+    {
+      now.push_back(nums[i]);
+      vis[i] = true;
+      find(nums);
+      now.pop_back();
+      vis[i] = false;
+    }
+  }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        memset(vis,false,sizeof(vis));
+        n = nums.size();
+        find(nums);
+        return ans;
+    }
+};
