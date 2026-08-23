8b7fd6c8bc3cc710b6b998f8920d82d2def69cac
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-08-23 14:08:48 +0600

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+const int mx = 5;
+class Solution {
+public:
+int n,m;
+bool vis[mx][mx];
+vector<pair<int,int>>jabo = {{1,0},{-1,0},{0,1},{0,-1}};
+string s;
+bool find(int i,int j,int ind,vector<vector<char>>& board)
+{
+  int nn,mm;
+  bool ok = false;
+  if(ind == s.size()) return true;
+  for(auto it : jabo)
+  {
+    nn = i + it.first;
+    mm = j + it.second;
+    if((nn >= 0) && (nn < n) && (mm >= 0) && (mm < m) && 
+    board[nn][mm] == s[ind] && !vis[nn][mm])
+    {
+      vis[nn][mm] = 1;
+      ok |= find(nn,mm,ind + 1,board);
+      vis[nn][mm] = 0;
+    }
+  }
+  return ok;
+}
+    bool exist(vector<vector<char>>& board, string word) {
+    s = word;
+    n = board.size();
+    m = board[0].size();
+  memset(vis,0,sizeof(vis));
+  bool ans = false;
+  for(int i = 0;i<n;i++)
+  {
+    for(int j = 0;j<m;j++)
+    {
+      if(board[i][j] == s[0])
+      {
+        vis[i][j] = 1;
+        ans |= find(i,j,1,board);
+        vis[i][j] = 0;
+        if(ans) break;
+      }
+    }
+    if(ans) break;
+  }
+  return ans;
+    }
+};
