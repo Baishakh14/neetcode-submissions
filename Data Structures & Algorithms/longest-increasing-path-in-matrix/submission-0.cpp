b4cc02b5843c4c05bb948b75fe6fca713cdb44aa
Mahfiz Rahman Baishakh <146338298+Baishakh14@users.noreply.github.com>
2026-09-15 02:13:10 +0600

    Add: longest-increasing-path-in-matrix - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+vector<vector<int>>dp;
+static const int mx = 110;
+int n,m;
+vector<pair<int,int>>movement = {{1,0},{-1,0},{0,1},{0,-1}};
+int find(int i,int j,vector<vector<int>>&ab)
+{
+    if(dp[i][j] != -1) return dp[i][j];
+    int ans = 1;
+    int now = 0;
+    int final = 1;
+    for(auto it : movement)
+    {
+        int xx = i + it.first;
+        int yy = j + it.second;
+        if(xx >= 0 && xx < n && yy >=0 && yy < m && ab[xx][yy]
+           > ab[i][j])
+           {
+               now = ans + find(xx,yy,ab);
+               final = max(final,now);
+           }
+    }
+    return dp[i][j] = final;
+
+}
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        dp.assign(mx,vector<int>(mx,-1));
+        n = matrix.size();
+        m = matrix[0].size();
+        int ans = 0;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<m;j++)
+            {
+                ans = max(ans,find(i,j,matrix));
+            }
+        }
+        return ans;
+    }
+};
